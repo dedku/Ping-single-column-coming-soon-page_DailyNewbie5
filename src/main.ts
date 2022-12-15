6860de2ef7654c096cb2bf8ed2c5f39ef2c87be8
Patryk Szabat <szabat.patryk@gmail.com>
2022-12-15 16:37:13 +0100

    ts + css

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,22 @@
-export const test = 1
+export const input = document.querySelector('.form-input') as HTMLInputElement
+export const button = document.querySelector('.cta') as HTMLButtonElement
+const error = document.querySelector('.error-massage') as HTMLDivElement
+
+button.addEventListener('click', (e) => {
+  e.preventDefault();
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g
+  const inputValue = input.value.length > 0
+  const verifieEmail = input.value.match(regex)
+  if (!inputValue) {
+    error.innerHTML = `Email field is empty`
+    input.style.border = `1px solid hsl(354, 100%, 66%);`
+  }
+  if (inputValue && !verifieEmail) {
+    error.innerHTML = `Please provide a valid email address`
+    input.style.border = `1px solid hsl(354, 100%, 66%);`
+  }
+  if (verifieEmail) {
+    error.innerHTML = ``
+    input.style.border = `1px solid var(--primary-blue);`
+  }
+})
